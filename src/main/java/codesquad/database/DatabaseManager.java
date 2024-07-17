27ff895fd5338c391d39090402049a6bee8e7ad4
hellomatia <shuaigejp80@gmail.com>
2024-07-17 23:23:11 +0900

    CSV DataBase가 인식을 잘하게끔 CREATE 쿼리문 변경

--- src/main/java/codesquad/database/DatabaseManager.java
+++ src/main/java/codesquad/database/DatabaseManager.java
@@ -27,30 +27,30 @@ public class DatabaseManager {
              Statement stmt = conn.createStatement()) {
             logger.info("Initializing database: users");
             stmt.executeQuery("CREATE TABLE users (" +
-                    "user_id, " +
-                    "user_name, " +
-                    "password, " +
-                    "email, " +
+                    "user_id , " +
+                    "user_name , " +
+                    "password , " +
+                    "email , " +
                     "user_image_url )");
             logger.info("Initializing database: posts");
             stmt.executeQuery("CREATE TABLE posts (" +
-                    "id, " +
-                    "title, " +
-                    "user_id, " +
-                    "user_name, " +
-                    "image_url, " +
-                    "content, " +
-                    "created_at, " +
+                    "id , " +
+                    "title , " +
+                    "user_id , " +
+                    "user_name , " +
+                    "image_url , " +
+                    "content , " +
+                    "created_at , " +
                     "updated_at )");
 
             logger.info("Initializing database: comments");
             stmt.executeQuery("CREATE TABLE comments (" +
-                    "id, " +
-                    "post_id, " +
-                    "user_id, " +
-                    "user_name, " +
-                    "comment, " +
-                    "created_at, " +
+                    "id , " +
+                    "post_id , " +
+                    "user_id , " +
+                    "user_name , " +
+                    "comment , " +
+                    "created_at , " +
                     "updated_at )");
         } catch (SQLException e) {
             logger.error("Failed to initialize database", e);
