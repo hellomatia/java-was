03ed80ca72d68535327f4afb06bbec03496e6960
hellomatia <shuaigejp80@gmail.com>
2024-07-19 09:59:28 +0900

    HttpMethod어노테이션의 안의 Method를 ide상 명시적으로 보여주기 위해 default값 제거

--- src/main/java/codesquad/server/handler/annotation/HttpMethod.java
+++ src/main/java/codesquad/server/handler/annotation/HttpMethod.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface HttpMethod {
-    String value() default "GET";
+    String value();
 }
